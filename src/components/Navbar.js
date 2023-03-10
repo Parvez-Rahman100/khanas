ca8d4c6cff4c91872ebb3b639054b28a6e8e2d8f
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-03-10 17:09:04 +0600

    side menu created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
-import React from "react";
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-import { BsFillCartFill } from "react-icons/bs";
+import React, { useState } from "react";
+import {
+  AiFillTag,
+  AiOutlineCloseCircle,
+  AiOutlineMenu,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
+import { FaUserFriends, FaWallet } from "react-icons/fa";
+import { MdFavorite, MdHelp } from "react-icons/md";
+import { TbTruckDelivery } from "react-icons/tb";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
     <div className=" max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* Left Side  */}
       <div className=" flex items-center">
-        <div className=" cursor-pointer">
+        <div onClick={() => setMenu(!menu)} className=" cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className=" text-2xl sm:text-3xl lg:text-4xl px-2">
-          Best <span className=" font-bold">Khanas</span>
+          Best{" "}
+          <span className=" font-bold bg-black text-white rounded-xl px-2">
+            Khanas
+          </span>
         </h1>
         <div className=" hidden lg:flex items-center bg-gray-400 rounded-full p-1 text-[14px]">
           <p className=" bg-black text-white rounded-full p-1">Delivery</p>
@@ -32,7 +45,65 @@ const Navbar = () => {
         <BsFillCartFill size={20} className=" mr-2" /> Cart
       </button>
       {/* Mobile Menu  */}
-      <div className=" bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      {/* Over lay  */}
+      {menu ? (
+        <div className=" bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
+
+      {/* Side drawer  */}
+      <div
+        className={
+          menu
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
+        <AiOutlineCloseCircle
+          onClick={() => setMenu(!menu)}
+          size={30}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
+        <h2 className=" text-2xl p-4">
+          Best{" "}
+          <span className="font-bold bg-black text-white rounded-xl px-1">
+            Khanas
+          </span>
+        </h2>
+        <nav>
+          <ul className=" flex flex-col p-4 bg-black text-white rounded-sm">
+            <li className=" text-xl py-4 flex items-center">
+              <TbTruckDelivery size={25} className="mr-4" />
+              Orders
+            </li>
+            <li className=" text-xl py-4 flex items-center">
+              <MdFavorite size={25} className="mr-4" />
+              Favourites
+            </li>
+            <li className=" text-xl py-4 flex items-center">
+              <FaWallet size={25} className="mr-4" />
+              Wallet
+            </li>
+            <li className=" text-xl py-4 flex items-center">
+              <MdHelp size={25} className="mr-4" />
+              Help
+            </li>
+            <li className=" text-xl py-4 flex items-center">
+              <AiFillTag size={25} className="mr-4" />
+              Promotions
+            </li>
+            <li className=" text-xl py-4 flex items-center">
+              <BsFillSaveFill size={25} className="mr-4" />
+              Best One
+            </li>
+            <li className=" text-xl py-4 flex items-center">
+              <FaUserFriends size={25} className="mr-4" />
+              Invite Friends
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
