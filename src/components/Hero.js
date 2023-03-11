b89646ae3b680a6a624ebe300739738c2f673814
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-03-11 15:32:15 +0600

    hero section created

---
+++ src/components/Hero.js
@@ -0,0 +1,27 @@
+import React from "react";
+import foods from "../imgs/foods.jpg";
+
+const Hero = () => {
+  return (
+    <div className=" max-w-[1640px] mx-auto p-4">
+      <div className=" max-h-[500px] relative">
+        {/* Overlay Here  */}
+        <div className=" w-full h-full absolute text-gray-200 max-h-[500px] flex flex-col justify-center bg-black/40">
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            Find <span className=" text-orange-700">Your</span>
+          </h1>
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            Best <span className=" text-orange-700">Khanas </span>
+          </h1>
+        </div>
+        <img
+          className=" w-full max-h-[500px] object-cover"
+          src={foods}
+          alt="foods"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
