b395723f9dc28b6008d38833d6cea4087f6f964d
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-03-11 23:59:26 +0600

    cards created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Card from "./Card";
+
+const Cards = () => {
+  const data = [
+    {
+      id: 1,
+      p1: "Order your Khana",
+      p2: "Before stock out",
+      img: require("../imgs/card1.jpg"),
+    },
+    {
+      id: 2,
+      p1: "Order your Khana",
+      p2: "Before stock out",
+      img: require("../imgs/card2.jpg"),
+    },
+    {
+      id: 3,
+      p1: "Order your Khana",
+      p2: "Before stock out",
+      img: require("../imgs/card3.jpg"),
+    },
+  ];
+
+  return (
+    <div className=" max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/* Cards here  */}
+      {data.map((data) => (
+        <Card key={data.id} data={data} />
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
