b09705c3b413a8367162ecff64cae3144335a68c
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-03-16 16:58:59 +0600

    all foods card added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Cards from "./components/Cards";
+import Food from "./components/Food";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="px-12">
+    <div className=" px-2 lg:px-12">
       <Navbar />
       <Hero />
       <Cards />
+      <Food />
     </div>
   );
 };
--- src/components/Food.js
+++ src/components/Food.js
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { FoodApi } from "../FoodApi/FoodApi";
 
 const Food = () => {
-  return <div></div>;
+  const [foods] = useState(FoodApi);
+
+  return (
+    <div className="max-w-[1640px] m-auto px-4">
+      <h1 className="text-orange-800 font-bold text-center text-4xl">
+        Top Khanas From Our Side.
+      </h1>
+      {/* Filter Row  */}
+      <div className="flex flex-col lg:flex-row justify-between pb-12">
+        {/* Filter Type  */}
+        <div>
+          <p className="font-bold text-gray-700">Khanas Type</p>
+          <div className="flex justify-between flex-wrap">
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              All
+            </button>
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              Burger
+            </button>
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              Pizza
+            </button>
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              Salad
+            </button>
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              Chickens
+            </button>
+          </div>
+        </div>
+        {/* Filter Price  */}
+        <div>
+          <p className="font-bold text-gray-700">Khanas Price</p>
+          <div className="flex justify-between max-w-[390px] w-full">
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              $
+            </button>
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              $$
+            </button>
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              $$$
+            </button>
+            <button className="button text-orange-400 border-orange-600 hover:bg-orange-400 hover:text-white m-1 border rounded-xl px-2 ">
+              $$$$
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Display Foods  */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
+        {foods.map((item, index) => (
+          <div
+            key={index}
+            className="border rounded-xl shadow-lg hover:scale-110 duration-150 cursor-pointer"
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-[200px] object-cover rounded-t-md rounded-b-sm"
+            />
+            <div className="flex justify-between px-2 py-4 items-center">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-black text-white rounded-full px-2">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Food;
